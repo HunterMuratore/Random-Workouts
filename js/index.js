f1a90857d8d46cc83cb088dbbdfd063c451b98e7
Hunter Muratore <muratoreh@gmail.com>
2023-01-04 15:08:50 -0500

    Reverted to onchange dropdowns, added an email link, added Body Weight exercise logic

--- js/index.js
+++ js/index.js
@@ -2,32 +2,58 @@ var selectedExercise;
 var selectedIntensity;
 var selectedTraining;
 var exercise1;
-var sets1;
 var reps1;
 var exercise2;
-var sets2;
 var reps2;
 var exercise3;
-var sets3;
 var reps3;
 var exercise4;
-var sets4;
 var reps4;
 
 // Assigns the selected values of the dropdowns to their appropriate variables
-$( document ).ready(function() {
-    $("#exercise-dropdown").change(() => {
-        selectedExercise = $(this).find(":selected").val();
-    });
+// $( document ).ready(function() {
+//     $("#exercise-dropdown").change(() => {
+//         selectedExercise = $(this).find(":selected").val();
+//     });
 
-    $("#intensity-dropdown").change(() => {
-        selectedIntensity = $(this).find(":selected").val();
-    });
+//     $("#intensity-dropdown").change(() => {
+//         selectedIntensity = $(this).find(":selected").val();
+//     });
 
-    $("#training-dropdown").change(() => {
-        selectedTraining = $(this).find(":selected").val();
-    });
-});
+//     $("#training-dropdown").change(() => {
+//         selectedTraining = $(this).find(":selected").val();
+//     });
+// });
+
+// Allows user to submit a message only if all required fields are filled out
+// $( document ).getElementById("feedback").addEventListener("submit", function(e) {
+//     var form = this;
+//     if(form.name.value == "") {
+//       alert("Please enter your name");
+//       form.name.focus();
+//       e.preventDefault();
+//     } else if(form.email.value == "") {
+//       alert("Please enter a valid email address");
+//       form.email.focus();
+//       e.preventDefault();
+//     } else if(form.message.value == "") {
+//       alert("Please enter your feedback");
+//       form.message.focus();
+//       e.preventDefault();
+//     }
+//   }, false);
+
+function exerciseFunction(value) {
+    selectedExercise = value;
+}
+
+function intensityFunction(value) {
+    selectedIntensity = value;
+}
+
+function trainingFunction(value) {
+    selectedTraining = value;
+}
 
 // Returns a random element of the specified array
 function randomExercise(array) {
@@ -37,43 +63,67 @@ function randomExercise(array) {
 // Generates 4 random, unique exercises from the specified array
 function generateExercises(type) {
     exercise1 = randomExercise(type);
-    while (exercise2 === null || exercise2 === exercise1) {
+    while (exercise2 === undefined || exercise2 === exercise1) {
         exercise2 = randomExercise(type);
     }
-    while (exercise3 === null || exercise3 === exercise1 || exercise3 === exercise2) {
+    while (exercise3 === undefined || exercise3 === exercise1 || exercise3 === exercise2) {
         exercise3 = randomExercise(type);
     }
-    while (exercise4 === null || exercise4 === exercise1 || exercise4 === exercise2 || exercise4 === exercise3) {
+    while (exercise4 === undefined || exercise4 === exercise1 || exercise4 === exercise2 || exercise4 === exercise3) {
         exercise4 = randomExercise(type);
     }
 }
 
 // Code executed when Generate Workout button is click. Produces a random 4 exercise workout and displays it in an alert
 $("#generateButton").click(() => {
-    console.log(selectedExercise);
-    console.log(selectedIntensity);
-    console.log(selectedTraining);
     if (selectedExercise === undefined || selectedIntensity === undefined || selectedTraining === undefined) {
         return alert("Must choose one of each");
-    } else {
+    } else if ((selectedExercise === "arms" && selectedTraining == "body-weight") || (selectedExercise === "shoulders" && selectedTraining == "body-weight")) {
+        return alert(`We do not have a ${selectedTraining} ${selectedExercise} workout available. Please choose another combination.`);
+    } 
+    else {
         console.log("You chose a %s, %s, %s workout", selectedIntensity, selectedTraining, selectedExercise);
     }
 
-    if (selectedExercise === "chest") {
-        generateExercises(chestExercises);
-    } else if (selectedExercise === "legs") {
-        generateExercise(legExercises);
-    } else if (selectedExercise === "back") {
-        generateExercise(backExercises);
-    } else if (selectedExercise === "arms") {
-        generateExercise(armExercises);
-    } else if (selectedExercise === "shoulders") {
-        generateExercise(shoulderExercises);
-    } else if (selectedExercise === "core") {
-        generateExercise(coreExercises);
-    } else if (selectedExercise === "stretch") {
-        generateExercise(stretchExercises);
+    if (selectedIntensity === "intense" && selectedTraining === "body-weight") {
+        reps1 = 20;
+        reps2 = 20;
+        reps3 = 20;
+        reps4 = 20;
+    } else if (selectedIntensity === "medium" && selectedTraining === "body-weight") {
+        reps1 = 15;
+        reps2 = 15;
+        reps3 = 15;
+        reps4 = 15;
+    } else if (selectedIntensity === "light" && selectedTraining === "body-weight") {
+        reps1 = 10;
+        reps2 = 10;
+        reps3 = 10;
+        reps4 = 10;
     }
+
+    if (selectedExercise === "chest" && selectedTraining === "strength") {
+        generateExercises(chestExercises);
+    } else if (selectedExercise === "chest" && selectedTraining === "body-weight") {
+        generateExercises(chestBodyWeight);
+    } else if (selectedExercise === "legs" && selectedTraining === "strength") {
+        generateExercises(legExercises);
+    } else if (selectedExercise === "legs" && selectedTraining === "body-weight") {
+        generateExercises(legBodyWeight);
+    } else if (selectedExercise === "back" && selectedTraining === "strength") {
+        generateExercises(backExercises);
+    } else if (selectedExercise === "back" && selectedTraining === "body-weight") {
+        generateExercises(backBodyWeight);
+    } else if (selectedExercise === "arms") {
+        generateExercises(armExercises);
+    } else if (selectedExercise === "shoulders") {
+        generateExercises(shoulderExercises);
+    } else if (selectedExercise === "core") {
+        generateExercises(coreExercises);
+    }
+    // else if (selectedExercise === "stretch") {
+    //     generateExercises(stretchExercises);
+    // }
 
     if (selectedIntensity === "intense" && selectedTraining === "strength") {
         reps1 = intenseStrengthReps(exercise1);
@@ -81,20 +131,40 @@ $("#generateButton").click(() => {
         reps3 = intenseStrengthReps(exercise3);
         reps4 = intenseStrengthReps(exercise4);
         alert(`Your workout for today is:\n\n${exercise1}: 4 sets, ${reps1} reps\n${exercise2}: 4 sets, ${reps2} reps\n${exercise3}: 4 sets, ${reps3} reps\n${exercise4}: 4 sets, ${reps4} reps\n\nIt is suggested to use heavier weights for this strength-based workout`);
+        resetExercises();
     } else if (selectedIntensity === "medium" && selectedTraining === "strength") {
         reps1 = mediumStrengthReps(exercise1);
         reps2 = mediumStrengthReps(exercise2);
         reps3 = mediumStrengthReps(exercise3);
         reps4 = mediumStrengthReps(exercise4);
         alert(`Your workout for today is:\n\n${exercise1}: 4 sets, ${reps1} reps\n${exercise2}: 4 sets, ${reps2} reps\n${exercise3}: 4 sets, ${reps3} reps\n${exercise4}: 4 sets, ${reps4} reps\n\nIt is suggested to use comfortable weights for this strength-based workout`);
+        resetExercises();
     } else if (selectedIntensity === "light" && selectedTraining === "strength") {
         reps1 = lightStrengthReps(exercise1);
         reps2 = lightStrengthReps(exercise2);
         reps3 = lightStrengthReps(exercise3);
         reps4 = lightStrengthReps(exercise4);
-        alert(`Your workout for today is:\n\n${exercise1}: 4 sets, ${reps1} reps\n${exercise2}: 4 sets, ${reps2} reps\n${exercise3}: 4 sets, ${reps3} reps\n${exercise4}: 4 sets, ${reps4} reps\n\nIt is suggested to use lighter weights for this strength-based workout`);
+        alert(`Your workout for today is:\n\n${exercise1}: 3 sets, ${reps1} reps\n${exercise2}: 3 sets, ${reps2} reps\n${exercise3}: 3 sets, ${reps3} reps\n${exercise4}: 3 sets, ${reps4} reps\n\nIt is suggested to use lighter weights for this strength-based workout`);
+        resetExercises();
+    } else if (selectedIntensity === "intense" && selectedTraining === "body-weight") {
+        alert(`Your workout for today is:\n\n${exercise1}: 4 sets, ${reps1} reps\n${exercise2}: 4 sets, ${reps2} reps\n${exercise3}: 4 sets, ${reps3} reps\n${exercise4}: 4 sets, ${reps4} reps`);
+        resetExercises();
+    } else if (selectedIntensity === "medium" && selectedTraining === "body-weight") {
+        alert(`Your workout for today is:\n\n${exercise1}: 4 sets, ${reps1} reps\n${exercise2}: 4 sets, ${reps2} reps\n${exercise3}: 4 sets, ${reps3} reps\n${exercise4}: 4 sets, ${reps4} reps`);
+        resetExercises();
+    } else if (selectedIntensity === "light" && selectedTraining === "body-weight") {
+        alert(`Your workout for today is:\n\n${exercise1}: 3 sets, ${reps1} reps\n${exercise2}: 3 sets, ${reps2} reps\n${exercise3}: 3 sets, ${reps3} reps\n${exercise4}: 3 sets, ${reps4} reps`);
+        resetExercises();
     }
 });
+
+// Sets exercise variable back to undefined
+function resetExercises() {
+    exercise1 = undefined;
+    exercise2 = undefined;
+    exercise3 = undefined;
+    exercise4 = undefined;
+}
 
 // Suggested to use to heavier weights to push yourself - Okay to not be able to reach the amount of reps
 function intenseStrengthReps(exercise) {
@@ -206,6 +276,15 @@ const chest20reps = [
     "Decline Pushups",
 ];
 
+const chestBodyWeight = [
+    "Pushups",
+    "Wide Pushups",
+    "Close Pushups",
+    "Incline Pushups",
+    "Decline Pushups",
+    "Chest Dips"
+];
+
 const legExercises = [
     "Squats",
     "Leg Extensions",
@@ -247,6 +326,17 @@ const leg20reps = [
     "Calf Raises",
     "Jump Squats",
     "Farmers Walk"
+];
+
+const legBodyWeight = [
+    "Squats",
+    "Bulgarian Split Squat",
+    "Calf Raises",
+    "Lunges",
+    "Jump Squats",
+    "Pause Squats",
+    "Glute Bridges",
+    "Stair Climb"
 ];
 
 const armExercises = [
@@ -302,7 +392,7 @@ const backExercises = [
     "Wide Grip Pulldowns",
     "Pullups",
     "L Pullups",
-    "Bar Hang",
+    "Pullup Bar Hang",
     "Kettlebell Swings",
     "Barbell Deadlift",
     "Hex Bar Deadlift",
@@ -341,8 +431,16 @@ const back8reps = [
 ];
 
 const back20reps = [
-    "Bar Hang",
+    "Pullup Bar Hang",
     "Kettlebell Swings"
+];
+
+const backBodyWeight = [
+    "Pullup Bar Hang",
+    "Pullups",
+    "L Pullups",
+    "Lying Superman",
+    "Wide Pushups"
 ];
 
 const shoulderExercises = [
